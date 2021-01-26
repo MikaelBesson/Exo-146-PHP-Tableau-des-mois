e724ca1b447d1e033888569531cc9c65adad2e3a
BessonMikael <besson.mikael04@gmail.com>
2021-01-26 09:10:10 +0100

    exo 146 fini

--- index.php
+++ index.php
@@ -15,20 +15,20 @@ $mois = [
     "décembre",
 ];
 
-echo "<br>exercices 2<br>";
+echo "<br>exercices 2<br><br>";
 
 echo $mois[2]."<br>";
 
-echo "<br>exercices 3<br>";
+echo "<br>exercices 3<br><br>";
 
 echo $mois[5]."<br>";
 
-echo "<br>exercices 4<br>";
+echo "<br>exercices 4<br><br>";
 $search4 = "u";
 $result4 = str_replace("$search4","ù",$mois[7]);
 echo $result4."<br>";
 
-echo "<br>exercices 5<br>";
+echo "<br>exercices 5<br><br>";
 
 $nord = [
     59 => "nord",
@@ -38,23 +38,28 @@ $nord = [
     60 => "oise",
 ];
 
-echo "<br>exercices 6<br>";
+echo "<br>exercices 6<br><br>";
 echo $nord[59];
 
-echo "<br>exercices 7<br>";
+echo "<br>exercices 7<br><br>";
 $nord[51] = "marne";
 
-echo "<br>exercices 8<br>";
+echo "<br>exercices 8<br><br>";
 foreach ($mois as $moi){
     echo $moi."<br>";
 }
 
-echo "<br>exercices 9<br>";
+echo "<br>exercices 9<br><br>";
 foreach ($nord as $dept){
     echo $dept."<br>";
 }
 
-echo "<br>exercices 10<br>";
+echo "<br>exercices 10<br><br>";
+
+
+        foreach ($nord as $key => $value){
+            echo "Le departement ".$value." a le numero ".$key."<br>";
+}
 
 
 
